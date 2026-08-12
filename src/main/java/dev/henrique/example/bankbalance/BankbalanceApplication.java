237fca5ad5be5a49d105cb423b632ec3df178272
henrique-barnabe <henrique.barnabe@gmail.com>
2026-08-12 11:16:32 -0300

    initial commit

---
+++ src/main/java/dev/henrique/example/bankbalance/BankbalanceApplication.java
@@ -0,0 +1,13 @@
+package dev.henrique.example.bankbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankbalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankbalanceApplication.class, args);
+	}
+
+}
